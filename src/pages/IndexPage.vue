f47228a7b90bcf6a7fa0c88d3599d5c03e4eaf4e
Luisa C Lange <luisaclange@gmail.com>
2023-08-21 20:57:53 -0300

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row items-center justify-evenly bg-dark">
+    <div class="bg-grey q-px-xl rounded-borders">
+      <h3>Em breve novidades...</h3>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
